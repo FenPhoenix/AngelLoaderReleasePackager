06099a323b15dab05b85440b1cfc5e0b9e64e8d5
FenPhoenix <FenPhoenix@users.noreply.github.com>
2025-05-17 21:59:10 -0700

    Clarify comments

--- AngelLoaderReleasePackager/bat/git_server_update.bat
+++ AngelLoaderReleasePackager/bat/git_server_update.bat
@@ -1,5 +1,5 @@
 rem %1 must be the path of the AngelLoader updates repo and must be surrounded by quotes, eg., "C:\Users\Brian\source\repos\AngelLoaderUpdates"
-rem %2 must be the version, eg., "1.10.2"
+rem %2 must be the version (not surrounded by quotes), eg., 1.10.2
 cd %1
 git add --all
 git commit -m "Update info for version %2"
